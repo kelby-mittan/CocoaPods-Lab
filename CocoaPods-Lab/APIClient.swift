be7b11ababf4d16996986863092f9afcb0137ed6
Kelby Mittan <kelbymittan@pursuit.org>
2020-03-05 16:29:32 -0500

    Setting up files and creating API Client using AlamoFire

---
+++ CocoaPods-Lab/APIClient.swift
@@ -0,0 +1,37 @@
+//
+//  APIClient.swift
+//  CocoaPods-Lab
+//
+//  Created by Kelby Mittan on 3/5/20.
+//  Copyright © 2020 Kelby Mittan. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+struct APIClient {
+    
+    static func fetchUsers(completion: @escaping (AFResult<[User]>) -> ()) {
+        let endpointURLString = "https://randomuser.me/api/?results=50"
+        
+        guard let url = URL(string: endpointURLString) else {
+            return
+        }
+        
+        AF.request(url).response { (response) in
+            
+            if let error = response.error {
+                completion(.failure(error))
+            } else if let data = response.data {
+                do{
+                    let results = try JSONDecoder().decode(ResultsWrapper.self, from: data)
+                    let users = results.results
+                    completion(.success(users))
+                } catch {
+                    print("oops")
+                }
+            }
+        }
+    }
+    
+}
---
+++ CocoaPods-Lab/UsersDetailView.swift
@@ -0,0 +1,15 @@
+//
+//  UsersDetailView.swift
+//  CocoaPods-Lab
+//
+//  Created by Kelby Mittan on 3/5/20.
+//  Copyright © 2020 Kelby Mittan. All rights reserved.
+//
+
+import UIKit
+
+class UsersDetailView: UIView {
+
+    
+
+}
